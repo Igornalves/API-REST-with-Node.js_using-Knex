957360c7a8f1122de2e1d0c0be11c183b6150a71
igor_alves <igornalves08@gmail.com>
2024-03-07 11:30:05 -0300

    fazendo uma conecao usando knix junto com PG ee SQLITE

---
+++ src/databasePG.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable spaced-comment */
+import { knex as setupKnex } from 'knex'
+
+export const knex = setupKnex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'igorn',
+    password : '08901',
+    database : 'sistema_de_vendas'
+  }
+  // outra forma de fazer a conexao com o banco de dados 
+  // connection: 'postgres://igorn:08901@localhost:5432/sistema_de_vendas',
+  // searchPath: ['knex', 'public'],
+})
--- src/intex.ts
+++ src/intex.ts
@@ -1,15 +1,29 @@
 import fastify from 'fastify'
+import { knex } from './databasePG'
+import { knexTest } from './databaseSQLITE'
 
 const app = fastify()
 
 // base da aplicacao
-
 // principais metodos: GET, POST, PUT, PATCH, DELETE
-
 // http://localhost:3333/hello
 
-app.get('/hello', () => {
-  return 'hello node.js'
+app.get('/alimentos', async () => {
+  const tabelas = await knex('produtos_alimenticios').select('*')
+
+  return tabelas
+})
+
+app.get('/sqlite', async () => {
+  const tabelaTest = await knexTest('sqlite_schema').select('*')
+
+  return tabelaTest
+})
+
+app.get('/produtos', async () => {
+  const tabelas = await knex('produtos_eletronicos').select('*')
+
+  return tabelas
 })
 
 app
